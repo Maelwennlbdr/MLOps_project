5b9d9fcb80de3811eef6b44b25445dc1e7472516
Maelwenn Labidurie <maelwenn.labidurie@gmail.com>
2026-02-25 17:26:42 +0100

    test(api): add basic FastAPI tests for /health and /predict

---
+++ tests/test_api.py
@@ -0,0 +1,25 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_health():
+    res = client.get("/health")
+    assert res.status_code == 200
+    assert res.json() == {"status": "OK"}
+
+def test_predict():
+    sample = {
+        "Pregnancies": 6,
+        "Glucose": 148,
+        "BloodPressure": 72,
+        "SkinThickness": 35,
+        "Insulin": 0,
+        "BMI": 33.6,
+        "DiabetesPedigreeFunction": 0.627,
+        "Age": 50
+    }
+    res = client.post("/predict", json=sample)
+    assert res.status_code == 200
+    assert "prediction" in res.json()
+    assert "probability" in res.json()
